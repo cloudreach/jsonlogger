75a592a8b96e6e257eefb4694887ec92b182b2c5
John Houston <john.houston@cloudreach.co.uk>
2016-08-08 15:56:14 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='jsonlogger',
   packages=['jsonlogger'],
-  version='0.1.0',
+  version='0.1.1',
   description='zero conf JSON logging to standard output',
   author='John Houston',
   author_email='john.houston@cloudreach.com',
   url='https://github.com/cloudreach-pdev/jsonlogger',
-  download_url='https://github.com/cloudreach-pdev/jsonlogger/tarball/0.1.0',
+  download_url='https://github.com/cloudreach-pdev/jsonlogger/tarball/0.1.1',
   keywords=['logging', 'json'],
   classifiers=[],
 )
